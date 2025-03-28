1851b1775dce98227e9dca4a913fab055c36ee69
shritechno <shri.techno89@gmail.com>
2025-03-28 15:07:36 +0530

    Update config.php

--- admin/includes/config.php
+++ admin/includes/config.php
@@ -1,10 +1,9 @@
 <?php 
 // DB credentials.
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
+define('DB_HOST','13.201.229.211');
+define('DB_USER','userme');
+define('DB_PASS','#123456weare');
 define('DB_NAME','carrental');
-// Establish database connection.
 try
 {
 $dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
